feed031d435b73086689fd77445409a6e91ab6d9
KPdBest <115369690+KPdBest@users.noreply.github.com>
2022-10-09 00:00:52 +0530

    Update StrQ4.java

--- src/summertraining/StrQ4.java
+++ src/summertraining/StrQ4.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class StrQ4 {
     public static void main(String[] args) {
 	    Scanner x = new Scanner (System.in);
-	    int c=0;
+	    int count=0;
 	    System.out.println("Enter any String");
 	    String s=x.nextLine().toUpperCase();
 	    for (int i=0;i<s.length();i++)
 	    {
 	        if(s.charAt(i)=='A'|| s.charAt(i)=='E' || s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U')
-	        c+=1;
+	        count+=1;
 	        
 	    }
-		System.out.println("Number of Vowels in the String is/are: "+c);
+		System.out.println("Number of Vowels in the String is/are: "+count);
 	}
     
 }
